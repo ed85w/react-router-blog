5bc09d0c6199fed258a8e92ab8d54b24d500a2b3
ed85w <ed.walker@hotmail.co.uk>
2022-02-03 14:40:51 +0000

    complete except for about etc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
+import Footer from './Footer';
+import Nav from './Nav';
+import Home from './Home';
+import PostPage from './PostPage';
+import NewPost from './NewPost';
+import Missing from './Missing';
+import About from './About'
+
+import { Route, Switch, useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { format } from 'date-fns'
+
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ])
+
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const filteredPosts = posts.filter(post => 
+      post.body.toLowerCase().includes(search.toLowerCase()) || post.title.toLowerCase().includes(search.toLowerCase())
+    ) 
+    setSearchResults(filteredPosts.reverse()) // reverse used to show latest post first
+  }, [posts, search])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = {id, title: postTitle, datetime, body: postBody}; //create the new post 
+    const allPosts = [...posts, newPost] // add the new post to an array with the existing posts
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    history.push('/');
+  }
+
+  const handleDelete = (id) => {
+    // create a 'new' posts list without the selected post 
+    const postsList = posts.filter(post => post.id !== id);
+    // update posts to the new list 
+    setPosts(postsList);
+    history.push('/');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="React.js Blog"/>
+      <Nav search={search} setSearch={setSearch} />
+      <Switch>
+        <Route exact path="/">
+          <Home posts={searchResults}/>
+        </Route>
+        <Route exact path="/post">
+          <NewPost 
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
+          />
+        </Route>
+        <Route path="/post/:id">
+          <PostPage posts={posts} handleDelete={handleDelete}/>  
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+        <Missing />
+      </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/PostPage.js
@@ -0,0 +1,34 @@
+import { useParams, Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const PostPage = ({posts, handleDelete}) => {
+  const {id} = useParams(); //as defined in app.js route
+  const post = posts.find(post => (post.id).toString() === id);
+  return ( 
+    <main className="PostPage">
+      <article className="post">
+        {post && 
+          <>
+            <h2>{post.title}</h2>
+            <p className="postDate">{post.datetime}</p>
+            <p className="postBody">{post.body}</p>
+            <button
+              onClick={() => handleDelete(post.id)}
+            >
+              Delete post
+            </button>
+          </>
+        }
+
+        {!post &&
+        <>
+          <p>Oh dear! no post here</p>
+          <Link to="/">Back to home</Link>
+        </>
+        
+        }
+      </article>
+    </main>
+   );
+}
+ 
+export default PostPage;
